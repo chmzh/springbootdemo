a139827cd2bb75ff06876c049d785d6b28a8d5b5
chmzh <28458942@qq.com>
2018-02-25 11:50:07 +0800

    Update PersonDao.java

--- demo/src/main/java/com/example/hibernate/PersonDao.java
+++ demo/src/main/java/com/example/hibernate/PersonDao.java
@@ -12,4 +12,15 @@ public interface PersonDao extends JpaRepository<City, Long>{
     List<City> findByName(String name);
 
     City save(City city);
+    
+    @Query(value = "select * from city where id=:id" , nativeQuery = true)
+    City getCity(@Param("id") Long id);
+
+    @Query(value = "update city set name=?1 where id=?2 ", nativeQuery = true)
+    @Modifying
+    int updateName(String name,long id);
+
+    @Query(value = "delete from city1 where id=?1 ", nativeQuery = true)
+    @Modifying
+    int deleteCity(long id);
 }
